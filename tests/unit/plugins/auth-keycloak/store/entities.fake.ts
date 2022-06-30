2c2c86ea105364d8c3afd8751df3577334f7ac14
ruan.fachini <ruanvfachini@gmail.com>
2022-06-30 16:59:05 -0300

    mutation tests - finished

--- tests/unit/plugins/auth-keycloak/store/entities.fake.ts
+++ tests/unit/plugins/auth-keycloak/store/entities.fake.ts
@@ -3,10 +3,10 @@ import { AuthState, KeycloakAuth } from "@/plugins/auth-keycloak/auth.entities";
 export class StoreTestType {
   auth: AuthState;
 
-  constructor(token = new KeycloakAuth()) {
+  constructor(token = new KeycloakAuth(), lastUri = "") {
     this.auth = {
       token: token,
-      currentUri: ""
+      currentUri: lastUri
     };
   }
 };
--- tests/unit/plugins/auth-keycloak/store/mutations.spec.ts
+++ tests/unit/plugins/auth-keycloak/store/mutations.spec.ts
@@ -2,7 +2,7 @@ import { StoreTestType } from "./entities.fake";
 import { getStore } from "./mock.tools";
 import { cloneDeep } from "lodash";
 import { KeycloakAuth } from "@/plugins/auth-keycloak/auth.entities";
-import { SET_AUTH } from "@/plugins/auth-keycloak/store/keys";
+import { SET_AUTH, SET_LAST_URI } from "@/plugins/auth-keycloak/store/keys";
 
 const emptyToken = new KeycloakAuth(); 
 const someToken = new KeycloakAuth(1,"1",1,1,"1","1","1",1,"1");
@@ -10,14 +10,13 @@ const someToken = new KeycloakAuth(1,"1",1,1,"1","1","1",1,"1");
 describe("auth-plugin: store", () => {
   describe(SET_AUTH, () => {
     test.each([
-      { oldValue: emptyToken, newValue: someToken, expected: someToken },
-      { oldValue: someToken, newValue: emptyToken, expected: emptyToken },
-    ])(`oldAccessToken => $oldValue.access_token, newAceesToken => $newValue.access_token, currentAccessToken => $expected.access_token`, ({ oldValue, newValue, expected }) => {
+      { oldValue: emptyToken, newValue: someToken },
+      { oldValue: someToken, newValue: emptyToken },
+    ])(`oldAccessToken => \"$oldValue.access_token\", newAceesToken => \"$newValue.access_token\"`,
+    ({ oldValue, newValue }) => {
     
       //arrange
       const store = getStore(new StoreTestType(oldValue));
-      // const expected = store.state;
-      // const token = ;
       const initialState = cloneDeep(store.state);
     
       //act
@@ -25,7 +24,26 @@ describe("auth-plugin: store", () => {
     
       //assert
       expect(initialState).toEqual(new StoreTestType(oldValue));
-      expect(store.state.auth.token).toEqual(expected);
+      expect(store.state.auth.token).toEqual(newValue);
+    });
+  });
+  describe(SET_LAST_URI, () => {
+    test.each([
+      { oldValue: "/", newValue: "/login" },
+      { oldValue: "/login", newValue: "/home" },
+    ])(`oldLastUri => \"$oldValue\", newLastUri => \"$newValue\"`,
+    ({ oldValue, newValue }) => {
+    
+      //arrange
+      const store = getStore(new StoreTestType(null, oldValue));
+      const initialState = cloneDeep(store.state);
+    
+      //act
+      store.commit(SET_LAST_URI, newValue);
+    
+      //assert
+      expect(initialState).toEqual(new StoreTestType(null, oldValue));
+      expect(store.state.auth.currentUri).toEqual(newValue);
     });
   });
 });
